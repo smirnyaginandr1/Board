8be7d919bedc24633f901877dc5b7584d1cc4225
Andrey <smirnyaginandr27061998@gmail.com>
2021-04-18 13:38:18 +0500

    Add gitignore and clear (obj, debug)

--- src/WpfMath.Tests/obj/Debug/net461/.NETFramework,Version=v4.6.1.AssemblyAttributes.fs
+++
@@ -1,3 +0,0 @@
-namespace Microsoft.BuildSettings
-                [<System.Runtime.Versioning.TargetFrameworkAttribute(".NETFramework,Version=v4.6.1", FrameworkDisplayName=".NET Framework 4.6.1")>]
-                do ()
